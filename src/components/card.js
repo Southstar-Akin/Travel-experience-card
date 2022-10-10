eecc330877835b2d3ef3731d0718302039314d71
Southstar <akinbileoluwaseyi2001@gmail.com>
2022-10-10 10:05:49 +0100

    Nav component finished, Hero d\section to go

---
+++ src/components/card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./nav";
+
+function Card(){
+  return(
+    <div className=" w-[550px] h-[704px] grid grid-flow-row bg-white">
+        <Navbar/>
+    </div>
+  )
+}
+
+
+export default Card
---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Navbar(){
+  return(
+    <div className=" grid h-[55px] w-full bg-[#f55a5a] place-content-center text-white before:content-earth grid-flow-col gap-x-[7px] before:inline-block">
+          my travel journal
+    </div>
+  )
+}
+
+export default Navbar
